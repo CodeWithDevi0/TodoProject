7c46a2b275c8416f1e2949a0c2f76bd1ec789635
DeviCode <jpaquinol44@gmail.com>
2024-10-10 06:51:33 +0800

    Update index.php

--- MainPages/About/index.php
+++ MainPages/About/index.php
@@ -1,7 +1,7 @@
 <?php
 include('../template/header.php');
 ?>
-<div id="carouselExampleCaptions" class="carousel slide" style="padding-top: 5rem">
+<div id="carouselExampleCaptions" class="carousel slide" style="padding-top: 4.5rem">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
