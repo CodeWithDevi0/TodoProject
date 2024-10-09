b778153a2ab82c02b0399ba91bc5fd433597d549
DeviCode <jpaquinol44@gmail.com>
2024-10-10 06:56:10 +0800

    Update in about page
    footer part for about page

--- MainPages/About/index.php
+++ MainPages/About/index.php
@@ -39,5 +39,72 @@ include('../template/header.php');
     <span class="visually-hidden">Next</span>
   </button>
 </div>
+
+
+<!-- Footer -->
+<div class="" style="padding-top: 7rem" id="foot">
+  <footer 
+          class="text-center text-lg-start text-white"
+          style="background-color: #1c2331"
+          >
+    <!-- Section: Social media -->
+    <section
+             class="d-flex justify-content-between p-3"
+             style="background-color: #6351ce"
+             >
+      <!-- Left -->
+      <div class="me-5">
+        <span>Get connected with us on social networks:</span>
+      </div>    
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="">
+      <div class="container text-center text-md-start mt-2">
+        <!-- Grid row -->
+        <div class="row mt-3 justify-content-between">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mb-2">
+            <!-- Content -->
+            <h3 class="text-uppercase fw-bold"><span style="color: #FDAF7B">Task</span>enture</h3>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 10rem; background-color: #7c4dff; height: 2px"
+                />
+            <p>
+            "Success is the sum of small efforts, repeated day in and day out.
+             Taking notes is just one of those small efforts that leads to big achievements.
+            </p>
+          </div>
+          <div class="col-md-4 col-lg-3 col-xl-3 mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold">Contacts:</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 80px; background-color: #7c4dff; height: 2px"
+                />
+            <p><i class="fas fa-home mr-3"></i> Email: <a href="#" class="text-white">Taskenture@gmail.com</a></p>
+            <p><i class="fas fa-envelope mr-3"></i> Facebook: <a href="#" class="text-white">Facebook.com/Taskenture</a></p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div
+         class="text-center p-3"
+         style="background-color: rgba(0, 0, 0, 0.2)"
+         >
+      © 2020 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/"
+         >MDBootstrap.com</a
+        >
+    </div>
+  </footer>
+  </div>
 </body>
 </html>
